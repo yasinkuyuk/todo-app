490f0652c8f5c9535846b6d6f6da6a813e316d99
yasinkuyuk <yasinkuyuk@gmail.com>
2021-07-23 23:21:17 +0300

    local storage logic added. button and table design developed.

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -4,27 +4,48 @@
     <div class="no-task" v-if="taskListSize === 0">There is no task.</div>
     <div class="tasks" v-else>
       <table class="completed-tasks">
-        <h3>completed</h3>
-        <tr v-for="taskItem in completedTasks" :key="taskItem.id" id="completed">
+        <caption>Completed Tasks</caption>
+        <thead>
+          <tr>
+            <td></td>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Created Date</td>
+            <td>Completed Time</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr  v-for="taskItem in completedTasks" :key="taskItem.id">
           <td>
-            <input type="checkbox" v-model="taskItem.status" />
+            <button type="checkbox" default="unchecked" @click="changeStatusDone(taskItem)"></button>
           </td>
-          <td>{{ taskItem.title }}</td>
-          <td>{{ taskItem.description }}</td>
-          <td>{{ taskItem.dateCreated }}</td>
-
+          <td id="completed">{{ taskItem.title }}</td>
+          <td id="completed">{{ taskItem.description }}</td>
+          <td id="completed">{{ taskItem.dateCreated }}</td>
+          <td>{{ taskItem.completedTime}}</td>
         </tr>
+        </tbody>   
       </table>
       <table class="tasks">
-        <h3>uncompleted</h3>
-        <tr v-for="taskItem in unCompletedTasks" :key="taskItem.id">
-          <td>
-            <input type="checkbox" default="unchecked" v-model="taskItem.status" />
-          </td>
-          <td>{{ taskItem.title }}</td>
-          <td>{{ taskItem.description }}</td>
-          <td>{{ taskItem.dateCreated }}</td>
-        </tr>
+        <caption>Uncompleted Tasks</caption>
+        <thead>
+          <tr>
+            <td></td>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Created Date</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="taskItem in unCompletedTasks" :key="taskItem.id">
+            <td>
+              <button type="checkbox" default="unchecked" @click="changeStatusDone(taskItem)"></button>
+            </td>
+            <td>{{ taskItem.title }}</td>
+            <td>{{ taskItem.description }}</td>
+            <td>{{ taskItem.dateCreated }}</td>
+          </tr>
+        </tbody>
       </table>
       
     </div>
@@ -71,13 +92,16 @@ export default {
       var day = String(date.getDate()).padStart(2, "0");
       var month = String(date.getMonth() + 1).padStart(2, "0");
       var year = date.getFullYear();
+      var hour = date.getHours();
+      var minute = date.getMinutes();
+      var second = date.getSeconds();
 
-      var today = day + "/" + month + "/" + year;
+      var today = day + "/" + month + "/" + year + " "+hour+":"+minute+":"+second;
       return today;
     },
     makeTaskItem() {
       const task = {
-        id: "",
+        completedTime: "",
         title: this.tempTitle,
         description: this.tempDescription,
         dateCreated: this.getDate(),
@@ -91,9 +115,25 @@ export default {
       this.tempDescription = "";
     },
     changeStatusDone(taskItem) {
+      taskItem.completedTime = this.getDate();
+      console.log("time: ",taskItem.completedTime);
       taskItem.status = !taskItem.status;
     },
   },
+  watch: {
+    taskList : {
+      deep : true,
+      handler(){
+        const taskList = this.taskList;
+        localStorage.setItem("taskList", JSON.stringify(taskList));
+      }
+    }
+  },
+  mounted(){
+    const items = localStorage.getItem("taskList");
+    const taskList = JSON.parse(items ? items : "[]")
+    this.taskList = taskList;
+  }
 };
 </script>
 
@@ -116,4 +156,17 @@ a {
   text-decoration: line-through;
   color: gray;
 }
+
+.completed-tasks {
+  align-content: center;
+}
+
+caption {
+  font-weight: bold;
+}
+
+thead {
+  font-weight: bold;
+}
+
 </style>
